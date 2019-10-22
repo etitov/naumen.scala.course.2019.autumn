a02c5cdbd06a9f5375ce22573ed46082a4b95900
max <vominyh@yandex.ru>
2019-10-22 21:13:17 +0500

    homework 2

--- homeworks/homework_2/src/main/scala/Main.scala
+++
@@ -1,8 +0,0 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    print(Exercises.sortByHeavyweight(
-      Map(
-        "Gadolinium" -> (3, 4.1), "Iron" -> (8, 12), "Silver" -> (8, 11), "Titanium" -> (12, 4.32),
-        "Silicon" -> (1, 3), "Krypton" -> (5, 12), "Zinc" -> (13, 7.13), "Cerium" -> (14, 6.1))))
-  }
-}
